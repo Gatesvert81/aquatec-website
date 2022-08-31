c227fc78ef4e8d7cda3b766b6930af8f776be23b
Gatesvert <mathiasmartey81@outlook.com>
2022-08-31 16:47:35 +0000

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: 'https://aquatec-website.vercel.app',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
+  
