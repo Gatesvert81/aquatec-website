ce146aeb588cf3e2977de1d7bf74c2be2ed73580
Gatesvert81 <mathiasmartey81@outlook.com>
2022-09-14 10:24:38 +0000

    search hidden

--- pages/store/index.js
+++ pages/store/index.js
@@ -34,7 +34,7 @@ function Store() {
                     Store
                 </motion.h1>
             </motion.div>
-            <div className="w-full p-5 flex justify-center">
+            {/* <div className="w-full p-5 flex justify-center">
                 <InstantSearch
                     searchClient={searchClient}
                     indexName="dev_aquatec_store"
@@ -72,7 +72,7 @@ function Store() {
                     </div>
 
                 </InstantSearch>
-            </div>
+            </div> */}
             <motion.div className='p-10 text-center' >
                 <motion.h4 {...textAnimate} >
                     Categories
